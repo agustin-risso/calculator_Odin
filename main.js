221546bb8b9a282ba60d0ce61699ff7b0f902e76
Agustín Risso <agustinrisso89@gmail.com>
2023-10-11 19:31:00 -0300

    coded index.html and style.css

---
+++ main.js
@@ -0,0 +1,4 @@
+const clear = document.querySelector("#clearBtn");
+const remove = document.querySelector("#deleteBtn");
+const operators = document.querySelector("#operatorBtn");
+const number = document.querySelector("#numberBtn");
